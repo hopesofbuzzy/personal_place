b5fc3da5312d4bfd990fc91dd40ef2c486116073
hopesofbuzzy <nurbaevdaniar0@gmail.com>
2025-11-14 20:10:06 +0500

    Изменения?

--- personal_place/personal_place/settings.py
+++ personal_place/personal_place/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
+load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-xljj8cxu=%#q682fu#$476m5rb9c2o7v#)4l-ujlmi2z2h#+jk"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
